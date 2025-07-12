19fbce55b145d5e8a4de70ef6feccaf23b59eb98
Luis Vega <131728936+LuisKingZX@users.noreply.github.com>
2025-07-11 19:14:22 -0600

    SCRUM-39,54

---
+++ src/app/components/calendar/calendar.component.ts
@@ -0,0 +1,23 @@
+
+import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-calendar',
+  templateUrl: './calendar.component.html',
+  styleUrls: ['./calendar.component.scss']
+})
+export class CalendarComponent {
+  googleCalendarUrl: SafeResourceUrl = '' as SafeResourceUrl;
+
+  constructor(private sanitizer: DomSanitizer) {
+    this.loadCalendar();
+  }
+
+  loadCalendar(): void {
+    const calendarSrc = 'https://calendar.google.com/calendar/embed?src=thymiaoficial%40gmail.com&ctz=America%2FCosta_Rica';
+    this.googleCalendarUrl = this.sanitizer.bypassSecurityTrustResourceUrl(calendarSrc);
+  }
+}
+
+
