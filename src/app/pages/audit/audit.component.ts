96873162245d83e4a9641e445d4ac571a5a69f8c
mvh404 <150404367+mvh404@users.noreply.github.com>
2025-08-09 23:35:29 -0600

    Update audit.component.ts

--- src/app/pages/audit/audit.component.ts
+++ src/app/pages/audit/audit.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { AuditListComponent } from "../../components/audit/audit-list/audit-list.component";
 
 @Component({
   selector: 'app-audit',
+  standalone : true,
   templateUrl: './audit.component.html',
-  styleUrls: ['./audit.component.css']
+  styleUrls: ['./audit.component.css'],
+  imports: [AuditListComponent]
 })
 export class AuditComponent { }
