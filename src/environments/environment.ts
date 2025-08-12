5a587b224557d7d83c54ddfa22c9654c460e9a23
mvh404 <150404367+mvh404@users.noreply.github.com>
2025-08-12 17:19:22 -0600

    Update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://thymia-backend.onrender.com',
-  //apiUrl: 'http://localhost:8080',
+  // apiUrl: 'https://thymia-backend.onrender.com',
+  apiUrl: 'http://localhost:8080',
   googleClientId: 'http://619045755504-lgpg98g8obov041scbs784jdd5f49uug.apps.googleusercontent.com',
   googleApiKey: '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',
   deepgramApiKey: '72bea028ca6f428f8c0c4e8bc409f542f78ea90d'
